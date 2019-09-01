7b22898af81722f6b34f0ed3ad65b1956dba7d11
Katherin <katherin@j.local>
2019-09-01 11:55:13 +0100

    Synopsis and cover in details page

--- src/Gallery/TvShow.js
+++ src/Gallery/TvShow.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-export default function TvShow({id, title, img}) {
+export default function TvShow({id, title, img, synopsis}) {
     return (
         <div className='cover' id={id}>
             <Link to={id} ><img src={img} alt={title} />
             <div className="overlay">
                 <h2>{title}</h2>
+            </div>
+            <div className='synopsis'>
+                <h3>{synopsis}</h3>
             </div>
             </Link>
         </div>

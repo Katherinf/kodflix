9141608ff06a5b4fc0896ff76fbc9e36eec7d8c6
katherinf <katherinfons@gmail.com>
2019-09-13 12:00:45 +0100

    #22 and #23

--- src/backend/App.js
+++ src/backend/App.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
-var tvshowList = require('./tvshow-list.json');
+const tvshowList = require('./tvshow-list.json');
+
+app.use(express.static(path.join(__dirname, '../../build')));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
 
 app.get('/rest/shows', (req, res) => res.send(tvshowList))
 
--- src/frontend/Gallery/Gallery.js
+++ src/frontend/Gallery/Gallery.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Gallery.css';
 import TvShow from './TvShow';
-import getGallery from './get-gallery';
 
-export default function Gallery() {
-  return (
-    <>
-      <h1 className="rowlist-text">TV SHOW</h1>
-      <div className="rowlist-container">
-        {getGallery().map(show => <TvShow 
-          key={show.id} 
-          id={show.id} 
-          title={show.title} 
-          img={show.img} />
-        )}
-      </div>
-    </>
-  );
+export default class Gallery extends Component {
+  constructor() {
+      super();
+      this.state = {
+         tvshowList: [],
+      }
+  }
+ fetchTvShows () {
+      fetch('/rest/shows')
+      .then(res => res.json())
+      .then(data => this.setState({tvshowList: data}))
+ }
+ componentDidMount() {
+     this.fetchTvShows();
+  }
+  render() {
+    return (
+      <>
+        <h1 className="rowlist-text">TV SHOW</h1>
+        <div className="rowlist-container">
+          {this.state.tvshowList.map(show => <TvShow 
+            key={show.id} 
+            id={show.id} 
+            title={show.title} 
+            img={show.img} />
+          )}
+        </div>
+      </>
+    );
+  }
 }
